1d28b4c42c7b8904150bfa6578ca63776646dc04
Sparsh Johri <bhatabhaiyya@gmail.com>
2019-08-07 23:53:27 -0700

    The actual program is in "Get_data_from_the_Kepler_page2" I realized that the page itself contains a csv file that has all of the EPIC ID's scanned in the campaign. I put that file into a dataframe, cleaned the data to remove EPID ID's with partial entries (which are entries that are missing a RA, DEC, or magnitude), and put the resulting data back into another csv file. This data is in Campaign_2_EPICS.

---
+++ Get_data_from_the_Kepler_page2.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug  7 20:10:39 2019
+
+@author: bhata
+"""
+
+from selenium import webdriver
+import pandas
+
+
+driver = webdriver.Chrome('C:\\Users\\bhata\\Documents\\K2fov\\Web Scraping\\chromedriver.exe') 
+#establishes the driver
+print("Driver established")
+#-------------------------------------------------------------------------------------------------------------
+#-------------------------------------------------------------------------------------------------------------
+driver.get('https://keplerscience.arc.nasa.gov/k2-approved-programs.html') #gets the page
+campaign = driver.find_elements_by_xpath("//*[contains(@id,'campaign-2')]")[0].get_attribute(('id'))
+path = '//*[@id="' + campaign +'"]/ul[1]/li[4]/a' #goes to the appropriate place in the page
+print("Path gotten")
+#-------------------------------------------------------------------------------------------------------------
+#-------------------------------------------------------------------------------------------------------------
+label = driver.find_elements_by_xpath(path)[0]
+URL = label.get_attribute("href") #gets the URL
+print("URL gotten")
+#-------------------------------------------------------------------------------------------------------------
+#-------------------------------------------------------------------------------------------------------------
+csv_file = pandas.read_csv(URL) #uses the URL to open the csv file into a pandas dataframe
+csv_file.set_index('EPIC ID', inplace=True, drop = True) #causes the EPIC ID's to become the indexes of the dataframe
+print("File read")
+#-------------------------------------------------------------------------------------------------------------
+#-------------------------------------------------------------------------------------------------------------
+for i in range(len(csv_file.columns)-1):
+    data = csv_file[csv_file[csv_file.columns[i]]!=" "]
+    #gets the subset of data that doesn't have an empty right ascension, declination, or magnitude
+print("Data filtered")
+#-------------------------------------------------------------------------------------------------------------
+#-------------------------------------------------------------------------------------------------------------
+export_csv = data.to_csv (r'Campaign_2_EPICS.csv') #exports the dataframe as a csv to a file
+print("File exported")
+#-------------------------------------------------------------------------------------------------------------
+#-------------------------------------------------------------------------------------------------------------
+print("End of Program")
+driver.quit() #closes the web page
---
+++ Looking_up_in_SIMBAD.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul 31 18:33:45 2019
+
+@author: bhata
+"""
+from selenium import webdriver
+
+driver = webdriver.Chrome('C:\\Users\\bhata\\Documents\\K2fov\\Web Scraping\\chromedriver.exe') 
+driver.get('https://keplerscience.arc.nasa.gov/k2-approved-programs.html') 
