a48e64dbf7db34b1e58655d15b6e1b75dcdc027d
Gabloo <gabloo@tantiv4.com>
2019-08-07 23:18:00 -0700

    code to check against Simbad

---
+++ astro-1.py
@@ -0,0 +1,73 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+This is a temporary script file.
+"""
+
+#works on Spyder
+
+from lightkurve import search_targetpixelfile
+
+import requests
+from bs4 import BeautifulSoup
+from selenium import webdriver
+import time
+import pandas as pd
+df=pd.read_csv("CodyTable.csv")
+epic_ids= df.iloc[ : ,0]
+
+driver = webdriver.Chrome('chromedriver.exe')
+
+pms= list()
+for id in epic_ids:
+    driver.get('http://simbad.u-strasbg.fr/simbad/sim-id?Ident=EPIC+'+str(id)+'&submit=submit+id') #get the page
+    try:
+        ids = driver.find_elements_by_xpath("//*[contains(@id,'basic_data')]") 
+        path = '//*[@id="' + ids[0].get_attribute('id') +'"]/table/tbody/tr/td/font' 
+        element = driver.find_elements_by_xpath(path) 
+        #print(element[0].text)
+        #print(' ')
+        if(element[0].text.find('T Tau-type Star') != -1 or 
+                element[0].text.find('Pre-main sequence Star') != -1 or
+                element[0].text.find('Emission-line Star') != -1 or
+                element[0].text.find('Young Stellar Object') != -1 or
+                element[0].text.find('Flare Star') != -1 or
+                element[0].text.find('Variable Star of Orion Type') != -1 or
+                element[0].text.find('Brown Dwarf') != -1 or
+                element[0].text.find('Star in Association') != -1
+                ):
+            print( 'EPIC ',id,' is a PMS star')
+            pms.append(id)
+    except IndexError:
+        print('not found')    
+print( 'PMS stars found: ',len(pms)) 
+'''
+driver.get('http://simbad.u-strasbg.fr/simbad/sim-id?Ident=EPIC+2037866955&submit=submit+id') #get the page
+try:
+    ids = driver.find_elements_by_xpath("//*[contains(@id,'basic_data')]") 
+    path = '//*[@id="' + ids[0].get_attribute('id') +'"]/table/tbody/tr/td/font' 
+    element = driver.find_elements_by_xpath(path) 
+    print(element[0].text)
+except IndexError:
+    print('not found')
+
+
+#return structure fo the targetpixelfile
+#http://docs.lightkurve.org/api/lightkurve.targetpixelfile.KeplerTargetPixelFile.html#lightkurve.targetpixelfile.KeplerTargetPixelFile
+tpf = search_targetpixelfile(203382255).download();
+print("Mission:",tpf.mission,"  Campaign:", tpf.campaign, "  Tahrget:KIC", tpf.targetid)
+print('Quality',tpf.quality)
+print('cadence no', tpf.cadenceno)
+#print(tpf.show_properties())
+
+
+#tpf.plot(frame=1)
+
+#integrate each frame with "aperture method"
+#and create a time series of intensity
+lc = tpf.to_lightcurve();
+lc.plot()
+'''
+
+driver.quit()
+print('Done')
